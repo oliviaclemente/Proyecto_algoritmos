9b90b0b9ed08aa1e939dd2f506451edf7d466df0
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2024-05-07 09:57:08 +0200

    cc

--- resolucion_sudoku1.py
+++ resolucion_sudoku1.py
@@ -15,3 +15,40 @@ def main():
     sudoku.print_board()
     
 main()
+
+class SudokuSolver:
+    def __init__(self, board):
+        self.board = board
+
+    def solve(self):
+        empty_cell = self.find_empty_cell()
+        if not empty_cell:
+            return True
+        row, col = empty_cell
+        for num in range(1, 10):
+            if self.is_valid_move(row, col, num):
+                self.board[row][col] = num
+                if self.solve():
+                    return True
+                self.board[row][col] = 0
+        return False
+
+    def is_valid_move(self, row, col, num):
+        for i in range(9):
+            if self.board[row][i] == num or self.board[i][col] == num:
+                return False
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(3):
+            for j in range(3):
+                if self.board[start_row + i][start_col + j] == num:
+                    return False
+        return True
+
+    def find_empty_cell(self):
+        for i in range(9):
+            for j in range(9):
+                if self.board[i][j] == 0:
+                    return (i, j)
+        return None
+
+    
